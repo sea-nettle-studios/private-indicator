7110da3211a57210181efb789502439b9806682e
sea nettle studios <dev@seanettlestudios.com>
2023-02-10 18:29:26 -0600

    update icons after new theme selection

--- options.js
+++ options.js
@@ -8,5 +8,18 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 document.querySelector('#icon-theme').addEventListener('input', async (event) => {
-    await browser.storage.sync.set({[STORAGE_KEY_ICON_THEME]: event.target.value});
+    const iconTheme = event.currentTarget.value;
+
+    await browser.storage.sync.set({[STORAGE_KEY_ICON_THEME]: iconTheme});
+
+    for (const windowInfo of await browser.windows.getAll()) {
+        const iconStatus = windowInfo.incognito ? 'on' : 'off';
+        const iconTitle = windowInfo.incognito ? 'private' : 'not private';
+
+        await browser.browserAction.setIcon({
+            path: `images/${iconTheme}-${iconStatus}.svg`,
+            windowId: windowInfo.id,
+        });
+        await browser.browserAction.setTitle({title: iconTitle, windowId: windowInfo.id});
+    }
 });
